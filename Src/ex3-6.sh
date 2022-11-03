556c0a1cab42b6febd3a6d85c8726d15202cbb7e
leejiyoung22 <101188022+leejiyoung22@users.noreply.github.com>
2022-11-04 01:37:44 +0900

    Update ex3-6.sh

--- Src/ex3-6.sh
+++ Src/ex3-6.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 fname=$1
-if ! [ -d $fname ]; then
+if ! [ -d $fname ]; then 
 	mkdir $fname
 fi
 
-touch file0.txt
-touch file1.txt
-touch file2.txt
-touch file3.txt
-touch file4.txt
+touch file0.txt file1.txt file2.txt file3.txt file4.txt
 
 mv *.txt $fname
 
-tar -cvf new_file.tar file0.txt file1.txt file2.txt file3.txt file4.txt file5.txt
+cd $fname
+tar -czvf files.tar file0.txt file1.txt file2.txt file3.txt file4.txt 
+
 mkdir new_files
-tar xvf newfiles.tar
+tar -xvf files.tar -C /home/jylee/$fname/new_files
+mv files.tar new_files
